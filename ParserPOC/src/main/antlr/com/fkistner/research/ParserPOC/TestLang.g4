aa47a27005b99fa2e42c29ac1eb54c11ea701716
Florian Kistner <mail@fkistner.com>
2017-07-08 01:54:38 +0200

    working lexer + parser

--- ParserPOC/src/main/antlr/com/fkistner/research/ParserPOC/TestLang.g4
+++ ParserPOC/src/main/antlr/com/fkistner/research/ParserPOC/TestLang.g4
@@ -1,13 +1,33 @@
 grammar TestLang;
 
-Whitespace
-    : [ \t\r\n]+ -> channel(HIDDEN)
-    ;
+@header {
+package com.fkistner.research.ParserPOC;
+}
+
+// Parser
 
 program
-        : print | program print
+        : statement* EOF
+        ;
+
+statement
+        : print
         ;
 
 print
-        :   'print' '"' ~('"')+ '"'
+        : Print String
         ;
+
+
+// Lexer
+
+Print: 'print';
+
+String: Quote StringChar* Quote;
+
+Quote: '"';
+
+fragment
+StringChar: ~["];
+
+Whitespace: [ \t\r\n]+ -> skip;
--- ParserPOC/src/main/kotlin/com/fkistner/research/ParserPOC/Controller.kt
+++ ParserPOC/src/main/kotlin/com/fkistner/research/ParserPOC/Controller.kt
@@ -5,11 +5,50 @@ package com.fkistner.research.ParserPOC
  */
 
 import com.fkistner.research.ParserPOC.*;
+import org.antlr.v4.runtime.*
+import java.io.StringReader
 
-class Controller {
+fun main(args : Array<String>) {
+    // CharStreams.fromString may cause crashes, see also https://github.com/antlr/antlr4/issues/1879
+    val charStream = CharStreams.fromReader(StringReader("""
+        print "a" print "b" print " "
+        print ""
+    """))
 
-    fun main() {
-        val lexer = TestLangLexer()
+    println("Lexer:")
+    val lexer = TestLangLexer(charStream)
+    lexer.addErrorListener(object : BaseErrorListener() {
+        override fun syntaxError(recognizer: Recognizer<*, *>?, offendingSymbol: Any?, line: Int, charPositionInLine: Int, msg: String?, e: RecognitionException?) {
+            e?.printStackTrace()
+        }
+    })
+
+    try {
+        do {
+            val token = lexer.nextToken()
+            if (token.type < 1) break
+            println("${lexer.ruleNames[token.type - 1]} '${token.text}'")
+        } while (true)
+    } catch (e: Exception) {
+        e.printStackTrace();
+    }
+    finally {
+        lexer.reset()
     }
 
+    println("Parser:")
+    val parser = TestLangParser(CommonTokenStream(lexer))
+    parser.addErrorListener(object : BaseErrorListener() {
+        override fun syntaxError(recognizer: Recognizer<*, *>?, offendingSymbol: Any?, line: Int, charPositionInLine: Int, msg: String?, e: RecognitionException?) {
+
+        }
+    })
+
+    val program = parser.program()
+    program.accept(object : TestLangBaseVisitor<Unit>() {
+        override fun visitPrint(ctx: TestLangParser.PrintContext?) {
+            println(ctx?.toStringTree())
+        }
+    })
+    println("Errors: ${parser.numberOfSyntaxErrors}")
 }
