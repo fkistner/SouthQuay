b21a661d60273d853c594a28e16ef4fba64f5a59
Florian Kistner <mail@fkistner.com>
2017-07-11 18:24:46 +0200

    Test structure of parse tree

--- src/test/kotlin/com/fkistner/SouthQuay/test/ParserTests.kt
+++ src/test/kotlin/com/fkistner/SouthQuay/test/ParserTests.kt
@@ -1,10 +1,22 @@
 package com.fkistner.SouthQuay.test
 
-import org.junit.*
 import com.fkistner.SouthQuay.grammar.*
+import com.fkistner.SouthQuay.grammar.SQLangParser.*
 import org.antlr.v4.runtime.*
-import org.antlr.v4.runtime.tree.TerminalNode
+import org.antlr.v4.runtime.tree.*
+import org.junit.*
 import java.io.StringReader
+
+/** Simplified tree of parsed rules and tokens */
+private sealed class TestTree
+/** Rule Node */
+private data class N(val ruleIdx: Int, var children: List<TestTree> = listOf()) : TestTree()
+/** Leaf Node */
+private data class L(val text: String) : TestTree()
+/** Error Node */
+private object Error : TestTree()
+/** EOF Node */
+private object EOF : TestTree()
 
 class ParserTests {
 
@@ -14,15 +26,38 @@ class ParserTests {
         }
     }
 
-    private fun bailingParserForString(testString: String): SQLangParser {
+    private fun parserForString(testString: String): Pair<SQLangLexer, SQLangParser> {
         val charStream = CharStreams.fromReader(StringReader(testString))
-
         val lexer = SQLangLexer(charStream)
-        lexer.addErrorListener(BailErrorListener)
         val parser = SQLangParser(CommonTokenStream(lexer))
-        parser.addErrorListener(BailErrorListener)
+        return Pair(lexer, parser)
+    }
 
+    private fun bailingParserForString(testString: String): SQLangParser {
+        val (lexer, parser) = parserForString(testString)
+        lexer.addErrorListener(BailErrorListener)
+        parser.addErrorListener(BailErrorListener)
         return parser
+    }
+
+    private fun toTestTree(program: ProgramContext) : TestTree {
+        return program.accept(object : SQLangBaseVisitor<TestTree>() {
+            override fun visitTerminal(node: TerminalNode): TestTree {
+                return if (node.symbol.type == Recognizer.EOF)
+                    EOF
+                else
+                    L(node.text)
+            }
+
+            override fun visitErrorNode(node: ErrorNode): TestTree {
+                return Error
+            }
+
+            override fun visitChildren(node: RuleNode): TestTree {
+                val children = (0 until node.childCount).map { node.getChild(it).accept(this) }
+                return N(node.ruleContext.ruleIndex, children)
+            }
+        })
     }
 
     @Test
@@ -34,8 +69,7 @@ class ParserTests {
         val program = parser.program()
 
         // Assert = no exception
-        Assert.assertEquals(1, program.childCount)
-        Assert.assertEquals(Token.EOF, (program.getChild(0) as? TerminalNode)?.symbol?.type)
+        Assert.assertEquals(N(RULE_program, listOf(EOF)), toTestTree(program))
     }
 
     @Test
@@ -47,8 +81,7 @@ class ParserTests {
         val program = parser.program()
 
         // Assert = no exception
-        Assert.assertEquals(1, program.childCount)
-        Assert.assertEquals(Token.EOF, (program.getChild(0) as? TerminalNode)?.symbol?.type)
+        Assert.assertEquals(N(RULE_program, listOf(EOF)), toTestTree(program))
     }
 
     @Test(expected = RecognitionException::class)
