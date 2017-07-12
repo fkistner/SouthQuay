3bb340aba073c1eeb0bec01517781e95a1be74f0
Florian Kistner <mail@fkistner.com>
2017-07-12 04:13:24 +0200

    Bare bones editor pane, closes #7

--- src/main/kotlin/com/fkistner/SouthQuay/Entrypoint.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/Entrypoint.kt
@@ -1,5 +1,13 @@
 package com.fkistner.SouthQuay
 
-fun main(args: Array<String>) { 
-  
+import com.fkistner.SouthQuay.UI.Editor
+import javax.swing.JFrame
+
+fun main(args: Array<String>) {
+    val frame = JFrame("Editor")
+    val editor = Editor()
+    frame.contentPane = editor.panel
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.pack()
+    frame.isVisible = true
 }
