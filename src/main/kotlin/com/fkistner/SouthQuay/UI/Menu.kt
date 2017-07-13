f1818846a50209536c37a4a6318b49bf5e05bf23
Florian Kistner <mail@fkistner.com>
2017-07-14 00:26:54 +0200

    Implement menu listener

--- src/main/kotlin/com/fkistner/SouthQuay/UI/Menu.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/UI/Menu.kt
@@ -5,7 +5,7 @@ import java.awt.event.*
 import javax.swing.*
 
 object Menu {
-    fun create(): JMenuBar {
+    fun create(menuListener: MenuListener? = null): JMenuBar {
         val menuShortcutKeyMask = Toolkit.getDefaultToolkit().menuShortcutKeyMask
         val menuBar = JMenuBar()
 
@@ -13,21 +13,26 @@ object Menu {
         fileMenu.mnemonic = KeyEvent.VK_F
 
         val newItem = fileMenu.add(JMenuItem("New…", KeyEvent.VK_N))
-        newItem.accelerator = KeyStroke.getKeyStroke('N', menuShortcutKeyMask)
+        newItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_N, menuShortcutKeyMask)
+        newItem.addActionListener { menuListener?.fileNew() }
 
         val openItem = fileMenu.add(JMenuItem("Open…", KeyEvent.VK_O))
-        openItem.accelerator = KeyStroke.getKeyStroke('O', menuShortcutKeyMask)
+        openItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_O, menuShortcutKeyMask)
+        openItem.addActionListener { menuListener?.fileOpen() }
 
         fileMenu.addSeparator()
 
         val closeItem = fileMenu.add(JMenuItem("Close", KeyEvent.VK_C))
-        closeItem.accelerator = KeyStroke.getKeyStroke('W', menuShortcutKeyMask)
+        closeItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_W, menuShortcutKeyMask)
+        closeItem.addActionListener { menuListener?.fileClose() }
 
         val saveItem = fileMenu.add(JMenuItem("Save", KeyEvent.VK_S))
-        saveItem.accelerator = KeyStroke.getKeyStroke('S', menuShortcutKeyMask)
+        saveItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_S, menuShortcutKeyMask)
+        saveItem.addActionListener { menuListener?.fileSave() }
 
         val saveAsItem = fileMenu.add(JMenuItem("Save As…", KeyEvent.VK_A))
-        saveAsItem.accelerator = KeyStroke.getKeyStroke('S', menuShortcutKeyMask or Event.ALT_MASK)
+        saveAsItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_S, menuShortcutKeyMask or Event.ALT_MASK)
+        saveAsItem.addActionListener { menuListener?.fileSaveAs() }
 
         return menuBar
     }
---
+++ src/main/kotlin/com/fkistner/SouthQuay/UI/MenuListener.kt
@@ -0,0 +1,9 @@
+package com.fkistner.SouthQuay.UI
+
+interface MenuListener {
+    fun fileNew()    {}
+    fun fileOpen()   {}
+    fun fileClose()  {}
+    fun fileSave()   {}
+    fun fileSaveAs() {}
+}
