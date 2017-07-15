5f758f53f4e69000ee57c56c630eae61474c6ee2
Florian Kistner <mail@fkistner.com>
2017-07-15 15:56:09 +0200

    Implement map function for interpreter

--- src/main/kotlin/com/fkistner/SouthQuay/interpreter/functions/MapFunction.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/interpreter/functions/MapFunction.kt
@@ -1,11 +1,15 @@
 package com.fkistner.SouthQuay.interpreter.functions
 
+import com.fkistner.SouthQuay.interpreter.values.SequenceValue
 import com.fkistner.SouthQuay.parser.Type
 
 object MapFunction : TypedInvocableFunction {
     override val type get() = Type.Sequence
 
     override fun invoke(args: List<Any?>): Any? {
-        TODO("not implemented")
+        val lambda = args[1] as InvocableFunction?
+        val sequence = args[0] as SequenceValue?
+        if (lambda == null || sequence == null) return null
+        return SequenceValue { sequence.stream().map { lambda(listOf(it)) as Int } }
     }
 }
--- src/test/kotlin/com/fkistner/SouthQuay/interpreter/InterpreterTests.kt
+++ src/test/kotlin/com/fkistner/SouthQuay/interpreter/InterpreterTests.kt
@@ -230,4 +230,27 @@ class InterpreterTests {
 
         Assert.assertEquals(2, participant.statementCounter)
     }
+
+    @Test
+    fun mapSequence() {
+        val (program, _) = ASTBuilder.parseText("out map({1, 2 * 3}, i -> i*i)")
+        program!!
+
+        val participant = object : CountingParticipant() {
+            override fun output(statement: Statement, string: String) {
+                Assert.assertEquals(1, statementCounter)
+                Assert.assertEquals("{1, 4, 9, …, 36}", string)
+            }
+
+            override fun statementExecuting(statement: Statement) {
+                super.statementExecuting(statement)
+                Assert.assertEquals(1, statementCounter)
+                Assert.assertTrue("Wrong reference.", program.statements[0] === statement)
+            }
+        }
+        val interpreter = Interpreter(participant)
+        interpreter.execute(program)
+
+        Assert.assertEquals(1, participant.statementCounter)
+    }
 }
