0679c613506ffb0eca5d454cdf738e7465e39688
Florian Kistner <mail@fkistner.com>
2017-07-15 16:04:42 +0200

    Implement reduce function for interpreter

--- src/main/kotlin/com/fkistner/SouthQuay/interpreter/functions/ReduceFunction.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/interpreter/functions/ReduceFunction.kt
@@ -1,11 +1,16 @@
 package com.fkistner.SouthQuay.interpreter.functions
 
+import com.fkistner.SouthQuay.interpreter.values.SequenceValue
 import com.fkistner.SouthQuay.parser.Type
 
 object ReduceFunction: TypedInvocableFunction {
     override val type get() = Type.Integer
 
     override fun invoke(args: List<Any?>): Any? {
-        TODO("not implemented")
+        val sequence = args[0] as SequenceValue?
+        val initial = args[1] as Int?
+        val lambda = args[2] as InvocableFunction?
+        if (lambda == null || initial == null || sequence == null) return null
+        return sequence.stream().reduce(initial) { state,n -> lambda(listOf(state, n)) as Int }
     }
 }
--- src/test/kotlin/com/fkistner/SouthQuay/interpreter/InterpreterTests.kt
+++ src/test/kotlin/com/fkistner/SouthQuay/interpreter/InterpreterTests.kt
@@ -253,4 +253,27 @@ class InterpreterTests {
 
         Assert.assertEquals(1, participant.statementCounter)
     }
+
+    @Test
+    fun reduceSequence() {
+        val (program, _) = ASTBuilder.parseText("out reduce({1, 10}, 5, a i -> a+i)")
+        program!!
+
+        val participant = object : CountingParticipant() {
+            override fun output(statement: Statement, string: String) {
+                Assert.assertEquals(1, statementCounter)
+                Assert.assertEquals("60", string)
+            }
+
+            override fun statementExecuting(statement: Statement) {
+                super.statementExecuting(statement)
+                Assert.assertEquals(1, statementCounter)
+                Assert.assertTrue("Wrong reference.", program.statements[0] === statement)
+            }
+        }
+        val interpreter = Interpreter(participant)
+        interpreter.execute(program)
+
+        Assert.assertEquals(1, participant.statementCounter)
+    }
 }
