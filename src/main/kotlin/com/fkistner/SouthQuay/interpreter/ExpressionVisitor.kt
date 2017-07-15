040e688a2ffbc8756e4c2243987a3013fae99be6
Florian Kistner <mail@fkistner.com>
2017-07-15 14:13:52 +0200

    Refactor expression visitor, turn invocable function into interface for proper casting checks

--- src/main/kotlin/com/fkistner/SouthQuay/interpreter/ExpressionVisitor.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/interpreter/ExpressionVisitor.kt
@@ -1,17 +1,12 @@
 package com.fkistner.SouthQuay.interpreter
 
-import com.fkistner.SouthQuay.interpreter.functions.InvocableFunction
+import com.fkistner.SouthQuay.interpreter.functions.*
 import com.fkistner.SouthQuay.parser.*
 
 
 class ExpressionVisitor(val context: ExecutionContext): ASTVisitor<Any> {
-    override fun visit(integerLiteral: IntegerLiteral): Int {
-        return integerLiteral.value
-    }
-
-    override fun visit(realLiteral: RealLiteral): Double {
-        return realLiteral.value
-    }
+    override fun visit(integerLiteral: IntegerLiteral) = integerLiteral.value
+    override fun visit(realLiteral: RealLiteral) = realLiteral.value
 
     private inline fun evaluate(binaryOperation: BinaryOperation, op: (Any, Any) -> Number?): Number? {
         val left  = binaryOperation.left.accept(this)
@@ -20,53 +15,43 @@ class ExpressionVisitor(val context: ExecutionContext): ASTVisitor<Any> {
         return op(left, right)
     }
 
-    override fun visit(sum: Sum): Number? {
-        return evaluate(sum) { left, right ->
-            when (sum.type) {
-                Type.Integer -> left as Int + right as Int
-                Type.Real    -> (left as Number).toDouble() + (right as Number).toDouble()
-                else -> throw IllegalStateException()
-            }
+    override fun visit(sum: Sum) = evaluate(sum) { left, right ->
+        when (sum.type) {
+            Type.Integer -> left as Int + right as Int
+            Type.Real    -> (left as Number).toDouble() + (right as Number).toDouble()
+            else -> throw IllegalStateException()
         }
     }
 
-    override fun visit(sub: Sub): Number? {
-        return evaluate(sub) { left, right ->
-            when (sub.type) {
-                Type.Integer -> left as Int - right as Int
-                Type.Real    -> (left as Number).toDouble() - (right as Number).toDouble()
-                else -> throw IllegalStateException()
-            }
+    override fun visit(sub: Sub) = evaluate(sub) { left, right ->
+        when (sub.type) {
+            Type.Integer -> left as Int - right as Int
+            Type.Real    -> (left as Number).toDouble() - (right as Number).toDouble()
+            else -> throw IllegalStateException()
         }
     }
 
-    override fun visit(mul: Mul): Number? {
-        return evaluate(mul) { left, right ->
-            when (mul.type) {
-                Type.Integer -> left as Int * right as Int
-                Type.Real    -> (left as Number).toDouble() * (right as Number).toDouble()
-                else -> throw IllegalStateException()
-            }
+    override fun visit(mul: Mul) = evaluate(mul) { left, right ->
+        when (mul.type) {
+            Type.Integer -> left as Int * right as Int
+            Type.Real    -> (left as Number).toDouble() * (right as Number).toDouble()
+            else -> throw IllegalStateException()
         }
     }
 
-    override fun visit(div: Div): Number? {
-        return evaluate(div) { left, right ->
-            when (div.type) {
-                Type.Integer -> left as Int / right as Int
-                Type.Real    -> (left as Number).toDouble() / (right as Number).toDouble()
-                else -> throw IllegalStateException()
-            }
+    override fun visit(div: Div) = evaluate(div) { left, right ->
+        when (div.type) {
+            Type.Integer -> left as Int / right as Int
+            Type.Real    -> (left as Number).toDouble() / (right as Number).toDouble()
+            else -> throw IllegalStateException()
         }
     }
 
-    override fun visit(pow: Pow): Number? {
-        return evaluate(pow) { left, right ->
-            when (pow.type) {
-                Type.Integer -> Math.pow((left as Int).toDouble(), (right as Int).toDouble()).toInt()
-                Type.Real    -> Math.pow((left as Number).toDouble(), (right as Number).toDouble())
-                else -> throw IllegalStateException()
-            }
+    override fun visit(pow: Pow) = evaluate(pow) { left, right ->
+        when (pow.type) {
+            Type.Integer -> Math.pow((left as Int).toDouble(), (right as Int).toDouble()).toInt()
+            Type.Real    -> Math.pow((left as Number).toDouble(), (right as Number).toDouble())
+            else -> throw IllegalStateException()
         }
     }
 
@@ -74,27 +59,24 @@ class ExpressionVisitor(val context: ExecutionContext): ASTVisitor<Any> {
         val from = sequence.from.accept(this)
         val to = sequence.to.accept(this)
         if (from == null || to == null) return null
-        val result = from as Int..to as Int
-        return result
+        return from as Int..to as Int
     }
 
-    override fun visit(variableRef: VariableRef): Any? {
-        return variableRef.declaration?.let { context.activeValues[it] }
-    }
+    override fun visit(variableRef: VariableRef) = variableRef.declaration?.let { context.activeValues[it] }
 
     override fun visit(functionInvoc: FunctionInvoc): Any? {
         val args = functionInvoc.args.map { it.accept(this) }
         return (functionInvoc.target as? InvocableFunction)?.invoke(args)
     }
 
-    override fun visit(lambda: Lambda): InvocableFunction {
-        return { args ->
+    override fun visit(lambda: Lambda) = object: InvocableFunction {
+        override fun invoke(args: List<Any?>): Any? {
             val innerContext = ExecutionContext()
             for ((idx, value) in args.withIndex()) {
                 innerContext.activeValues[lambda.parameters[idx]] = value
             }
             val visitor = ExpressionVisitor(innerContext)
-            lambda.body.accept(visitor)
+            return lambda.body.accept(visitor)
         }
     }
 }
--- src/main/kotlin/com/fkistner/SouthQuay/interpreter/functions/InvocableFunction.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/interpreter/functions/InvocableFunction.kt
@@ -2,5 +2,5 @@ package com.fkistner.SouthQuay.interpreter.functions
 
 import com.fkistner.SouthQuay.parser.Function
 
-typealias InvocableFunction = (List<Any?>) -> Any?
+interface InvocableFunction: (List<Any?>) -> Any?
 interface TypedInvocableFunction: InvocableFunction, Function
