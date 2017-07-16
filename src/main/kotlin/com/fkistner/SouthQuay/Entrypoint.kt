29a7b9af92450af23d777564401dafbb326c758a
Florian Kistner <mail@fkistner.com>
2017-07-16 14:42:27 +0200

    Set system look and feel

--- src/main/kotlin/com/fkistner/SouthQuay/Entrypoint.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/Entrypoint.kt
@@ -1,10 +1,12 @@
 package com.fkistner.SouthQuay
 
 import com.fkistner.SouthQuay.UI.Editor
+import javax.swing.UIManager
 
 fun main(args: Array<String>) {
     System.setProperty("apple.laf.useScreenMenuBar", "true")
     System.setProperty("apple.awt.application.name", ApplicationName)
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
 
     Editor()
 }
