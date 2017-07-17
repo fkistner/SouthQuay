a2e23e952f8263dfa27b5a0f82df89db8e9ed60c
Florian Kistner <mail@fkistner.com>
2017-07-17 02:21:13 +0200

    Correctly handle ⌘Q on macOS

--- src/main/kotlin/com/fkistner/SouthQuay/Entrypoint.kt
+++ src/main/kotlin/com/fkistner/SouthQuay/Entrypoint.kt
@@ -6,6 +6,7 @@ import javax.swing.*
 fun main(args: Array<String>) {
     System.setProperty("apple.laf.useScreenMenuBar", "true")
     System.setProperty("apple.awt.application.name", ApplicationName)
+    System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS")
     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
     UIManager.getDefaults().put("SplitPane.border", BorderFactory.createEmptyBorder())
 
